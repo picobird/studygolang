4043e337b82d213428e01c41a81d9c5ccc41e99a
picobird <dreamer.mzx@gmail.com>
2016-03-08 18:30:08 +0800

    python beautifulsoup4

--- test.py
+++ test.py
@@ -1,1 +1,42 @@
-print("hello, world!")
+# -- coding: utf-8 --
+import urllib.request
+import csv
+import re
+from bs4 import BeautifulSoup
+
+url = "http://idol001.com/xingcheng/list/star-exo-6730/"
+data = urllib.request.urlopen(url).read()
+data = data.decode('gbk', 'ignore').encode('utf-8')
+print(data)
+soup = BeautifulSoup(data, "lxml")
+travel_date = ""
+travel_time = ""
+travel_content = ""
+travel_type = ""
+travel_address = ""
+travel_res = ""
+table = soup.find("table", {"class": "schedule-table"})
+# print(table)
+
+with open('output.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+
+
+    td_th = re.compile('t[dh]')
+
+    for row in table.findAll("tr"):
+        cells = row.findAll(td_th)
+        if len(cells) == 6:
+            travel_date = cells[0].find(text=True)
+            travel_time = cells[1].find(text=True)
+            travel_content = cells[2].find(text=True)
+            travel_type = cells[3].find(text=True)
+            travel_address = cells[4].find(text=True)
+            travel_res = cells[5].find(text=True)
+
+            csv_writer.writerow([x.encode('utf-8') for x in [travel_date, travel_time,
+                                             travel_content, travel_type,
+                                             travel_address, travel_res]])
+
+
+f.close()
